bf573f3f172cf19e014a3b3a57cf1130ab996340
OneDuckyBoy <101095643+OneDuckyBoy@users.noreply.github.com>
2023-07-10 12:20:48 +0300

    Add camera fragment

--- app/src/main/java/com/example/edynamixapprenticeship/ui/MainActivity.java
+++ app/src/main/java/com/example/edynamixapprenticeship/ui/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.edynamixapprenticeship.R;
 import com.example.edynamixapprenticeship.ui.audio.AudioFragment;
+import com.example.edynamixapprenticeship.ui.camera.CameraFragment;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null)
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
-                    .add(R.id.fragment_container_view, AudioFragment.class, null)
+                    .add(R.id.fragment_container_view, CameraFragment.class, null)
                     .commit();
     }
 }
