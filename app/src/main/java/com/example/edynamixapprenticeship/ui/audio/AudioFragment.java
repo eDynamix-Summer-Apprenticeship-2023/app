653264d9ca7035898d8500dfa100890a5934bda1
Mihail Mihov <mihovmihailp@gmail.com>
2023-07-10 14:49:44 +0300

    Set recycler view layout manager programmatically

--- app/src/main/java/com/example/edynamixapprenticeship/ui/audio/AudioFragment.java
+++ app/src/main/java/com/example/edynamixapprenticeship/ui/audio/AudioFragment.java
@@ -14,7 +14,9 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.edynamixapprenticeship.R;
 import com.example.edynamixapprenticeship.databinding.AudioFragmentBinding;
 
 import dagger.hilt.android.AndroidEntryPoint;
@@ -30,6 +32,10 @@ public class AudioFragment extends Fragment {
                 }
             });
     private AudioAdapter adapter;
+
+    public AudioFragment() {
+        super(R.layout.audio_fragment);
+    }
 
     @Nullable
     @Override
@@ -48,7 +54,9 @@ public class AudioFragment extends Fragment {
         viewModel.getRecordings().observe(getViewLifecycleOwner(), recordings -> adapter.setRecordings(recordings));
         viewModel.getCurrentlyPlayingRecording().observe(getViewLifecycleOwner(), recording -> adapter.setCurrentlyPlayingRecording(recording));
 
+        binding.audioRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         binding.audioRecyclerView.setAdapter(adapter);
+
         binding.audioRecordFab.setOnClickListener(view -> {
             if (Boolean.FALSE.equals(viewModel.isRecording().getValue())) {
                 if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
