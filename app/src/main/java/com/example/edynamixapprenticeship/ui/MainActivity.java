e72b98531e284a6174bd31db9d0c589f854ebe2b
Kris4an <67221104+Kris4an@users.noreply.github.com>
2023-07-15 15:37:15 +0300

    start work on design

--- app/src/main/java/com/example/edynamixapprenticeship/ui/MainActivity.java
+++ app/src/main/java/com/example/edynamixapprenticeship/ui/MainActivity.java
@@ -1,26 +1,35 @@
 package com.example.edynamixapprenticeship.ui;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.edynamixapprenticeship.R;
-import com.example.edynamixapprenticeship.ui.audio.AudioFragment;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+    private void getMarsPhotos() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.weather_main_fragment);
+        TextView txt = findViewById(R.id.locationCurText);
+        txt.setText("Стара Загора");
+        TextView w = findViewById(R.id.weatherBg1).findViewById(R.id.cityName);
+        w.setText("Нова Загора");
+        TextView g = findViewById(R.id.weatherBg2).findViewById(R.id.cityName);
+        g.setText("Пловдив");
+//        if (savedInstanceState == null)
+//            getSupportFragmentManager().beginTransaction()
+//                    .setReorderingAllowed(true)
+//                    .add(R.id.fragment_container_view, AudioFragment.class, null)
+//                    .commit();
 
-        if (savedInstanceState == null)
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.fragment_container_view, AudioFragment.class, null)
-                    .commit();
     }
 }
---
+++ app/src/main/java/com/example/edynamixapprenticeship/ui/weather/WheatherMainFragment.java
@@ -0,0 +1,12 @@
+package com.example.edynamixapprenticeship.ui.weather;
+
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.edynamixapprenticeship.databinding.AudioFragmentBinding;
+
+public class WheatherMainFragment extends Fragment {
+
+}
