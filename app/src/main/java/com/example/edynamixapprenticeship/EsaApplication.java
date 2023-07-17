c89972b627df8322bfc5203899870c77a8b65a36
Mihail Mihov <mihovmihailp@gmail.com>
2023-07-17 09:48:52 +0300

    Configure Realm Device Sync

--- app/src/main/java/com/example/edynamixapprenticeship/EsaApplication.java
+++ app/src/main/java/com/example/edynamixapprenticeship/EsaApplication.java
@@ -1,13 +1,18 @@
 package com.example.edynamixapprenticeship;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.google.android.material.color.DynamicColors;
+
+import java.util.Objects;
 
 import dagger.hilt.android.HiltAndroidApp;
 import io.realm.Realm;
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.sync.SyncConfiguration;
 
 @HiltAndroidApp
 public class EsaApplication extends Application {
@@ -20,7 +25,20 @@ public class EsaApplication extends Application {
 
         Realm.init(this);
 
-        realmApp = new App(new AppConfiguration.Builder("application-0-ifkdv")
-                .build());
+        String realmAppID = "application-0-iynqf";
+
+        realmApp = new App(new AppConfiguration.Builder(realmAppID).build());
+
+        Credentials credentials = Credentials.anonymous();
+
+        realmApp.loginAsync(credentials, it -> {
+            if (it.isSuccess()) {
+                Log.e("AUTH", "Logged in as: " + it.get().getId());
+                SyncConfiguration config = new SyncConfiguration.Builder(Objects.requireNonNull(realmApp.currentUser())).build();
+                Realm.setDefaultConfiguration(config);
+            } else {
+                Log.e("AUTH", "Failed to log in: " + it.getError().getErrorMessage());
+            }
+        });
     }
 }
